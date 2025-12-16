15ca8edaf8d52c1cb6e70ea859cafa5037ba7aa0
Muhammad Zidane Fajarianto <muhammadzidanefaj@gmail.com>
2025-12-16 15:14:38 +0700

    fix: update config, htaccess, and build dist folder

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  // chainWebpack: config => {
-  //   config.module
-  //     .rule("pdf")
-  //     .test(/\.pdf/)
-  //     .use("file-loader")
-  //     .loader("file-loader");
-  //   }
+  publicPath: './',  // <--- TAMBAHKAN BARIS INI (Wajib!)
+  
+  // chainWebpack: config => { ... } (biarkan komentar ini kalau tidak dipakai)
 })
