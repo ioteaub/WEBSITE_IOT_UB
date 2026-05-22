7f1b6f6edb67723f21c0746b1247827ba834d7b7
Muhammad Zidane Fajarianto <muhammadzidanefaj@gmail.com>
2026-05-22 23:21:11 +0700

    build: update vue.config.js to output index.html to root

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: './',  // <--- TAMBAHKAN BARIS INI (Wajib!)
-  
+  publicPath: './dist/',
+  indexPath: '../index.html',
+
   // chainWebpack: config => { ... } (biarkan komentar ini kalau tidak dipakai)
 })
